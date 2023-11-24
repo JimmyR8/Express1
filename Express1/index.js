380c92e2b353aba7115538f885625d6fef2ae00d
Jimmy <jimmyjramirezg08@gmail.com>
2023-11-23 23:24:36 -0500

    CRUD-API+Key-authentication

--- Express1/index.js
+++ Express1/index.js
@@ -6,6 +6,33 @@ const port = 3000;
 app.use(express.json());
 //Decir en la pagina que la App ya esta funcionando
 app.listen(port, () => console.log(`The App is ON`));
+//23/11/2023
+//llamar la libreria//Cargar las variables de entorno desde el archivo (.env)
+require('dotenv').config();
+
+//Key authentication
+//const API_KEY = "holacomoestaslacontrasenaesesta"; //BAD PRACTICE!
+
+const contrasenaApi = process.env.API_KEY; //GOOD PRACTICE!
+// Middleware para verificar la API key
+const verificarApiKey = (req, res, next) => {
+    const apiKey = req.get('x-api-key'); // Suponiendo que la clave se pasa en el encabezado 'contrasena'
+
+    // Verificar si la clave de API es válida (puedes implementar tu propia lógica aquí)
+    if (apiKey === contrasenaApi) {
+        next(); // Continuar con la siguiente función en la cadena de middleware
+    } else {
+        res.status(401).send('Acceso no autorizado');
+    }
+};
+
+// Aplicar el middleware a todas las rutas que requieren la API key
+app.use('/students', verificarApiKey);
+app.use('/students/:id', verificarApiKey);
+app.use('/insertStudents', verificarApiKey);
+app.use('/updateStudents/:id', verificarApiKey);
+app.use('/delete/:id', verificarApiKey);
+
 
 
 //Mostrar en la Pagina el "Hola mundo" en diferente idiomas usando el "/" y la letra principal del idioma (EJEMPLO: I=Ingles, P=Portugues, A=Arabe)
@@ -99,6 +126,7 @@ const pool = new Pool({
     port: 5432,
     ssl: { rejectUnauthorized: false }
 });
+
 //GET Method (Obtener todos los usuarios de la Arrey de la base de datos)
 app.get('/students', function (req, res) {
     const listUsersQuery = `SELECT * FROM students;`;
@@ -107,11 +135,9 @@ app.get('/students', function (req, res) {
         .then(data => {
             console.log("List students: ", data.rows);
             res.send(data.rows);
-            pool.end();
         })
         .catch(err => {
             console.error(err);
-            pool.end();
         });
 });
 
@@ -127,7 +153,6 @@ app.get('/student/:id', function (req, res) {
         })
         .catch(err => {
             console.error(err);
-            pool.end();
         });
 });
 
@@ -140,11 +165,12 @@ app.post('/insertStudents', (req, res) => {
             console.log("List students: ", data.rows);
             return res.send("Usuario creado");
         })
-
-    console.log(res.rows)
+        .catch(err => {
+            console.error(err);
+        });
 });
 
-//UPDATE Method (Actulizar un archivo en especifico)
+//PATCH Method (Actulizar un archivo en especifico)
 app.patch('/updateStudents/:id', (req, res) => {
     const update = `UPDATE students SET id = ${req.body.id}, name = '${req.body.name}', lastname = '${req.body.lastname}', notes = '${req.body.notes}' WHERE id = ${req.params.id}`
 
@@ -153,24 +179,35 @@ app.patch('/updateStudents/:id', (req, res) => {
             console.log("List students: ", data.rows);
             return res.send("Usuario modificado");
         })
+        .catch(err => {
+            console.error(err);
+        });
 });
 
 //DELETE Method (Elminar un archivo en especifico)
 app.delete('/delete/:id', function (req, res) {
     const eliminar = `DELETE FROM students WHERE id = ${req.params.id}`
     pool.query(eliminar)
-    .then(data => {
-        console.log("List students: ", data.rows);
-        return res.send("Usuario eliminado");
-    })
+        .then(data => {
+            console.log("List students: ", data.rows);
+            return res.send("Usuario eliminado");
+        })
+        .catch(err => {
+            console.error(err);
+        });
 });
 
 //(Eliminar todos los usuarios)
 app.delete('/delete/users', function (req, res) {
     const eliminar = `DELETE FROM students`
     pool.query(eliminar)
-    .then(data => {
-        console.log("List students: ", data.rows);
-        return res.send("Usuarios eliminados");
-    })
-}); 
+        .then(data => {
+            console.log("List students: ", data.rows);
+            return res.send("Usuarios eliminados");
+        })
+        .catch(err => {
+            console.error(err);
+        });
+});
+
+//Deploy DATA BASE
